285bcb5a13d2aed18bbb214209cc3519fa6a4deb
Zhaoyan <zhaoyan.fan@uzh.ch>
2026-05-10 19:10:57 +0200

    Delete official_baselines/process_llm_results.py

--- official_baselines/process_llm_results.py
+++
@@ -1,5 +0,0 @@
-"""Placeholder for official BRIGHTER result post-processing script."""
-
-
-def process_results(*_args, **_kwargs):
-    raise NotImplementedError("Use the official BRIGHTER baseline script in this location.")
